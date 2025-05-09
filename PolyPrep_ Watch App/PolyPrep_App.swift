605ed9090f252fa5909e9a48a397ed975e073bdb
miniBObr <inttv2003@yandex.ru>
2025-05-09 14:14:15 +0300

    увязал телефон и часа, ща попробуем писать приложение

---
+++ PolyPrep_ Watch App/PolyPrep_App.swift
@@ -0,0 +1,17 @@
+//
+//  PolyPrep_App.swift
+//  PolyPrep_ Watch App
+//
+//  Created by boss on 09.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct PolyPrep__Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
